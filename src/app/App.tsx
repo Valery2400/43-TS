11b155852c6d2d0df7fcc82eb8c8266cdedfe124
Valerii Kuchynskyi <valeryk@hotmail.com>
2024-07-10 17:29:50 +0200

    second commit

---
+++ src/app/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, typescript react ⛵</h1>
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ src/lessons/lesson06/Lesson06.tsx
@@ -0,0 +1,97 @@
+import { log } from "console";
+
+function Lesson06() {
+  // 1. String
+
+  let username: string = "Bob";
+  username = "Harry";
+  console.log(username);
+
+
+// 2.number
+let number: number = 42;
+number = 7.1;
+number = NaN;
+number = 2 + Number('1')
+console.log(number);;
+
+// 3.boolean
+
+let isAdmin : boolean = false
+isAdmin = true
+isAdmin = 2 > 3
+console.log(isAdmin);
+
+// * null / undefined
+
+let emptyValue: null = null;
+// emptyValue = 0
+console.log(emptyValue);
+
+let empty: undefined = undefined;
+console.log(empty);
+
+// * arrays
+
+const colors: string[] = ['red', 'green', 'blue'];
+// colors.push(1)
+colors.push("");
+colors[3] = 'yellow'
+console.log(colors);
+
+// * functions
+
+const showMessage =(): void => {
+    console.log('hello');
+}
+
+showMessage()
+
+const sum = (a:number,b:number):number => {
+    return a + b
+}
+console.log(sum(4,10));
+
+// * objects interface
+
+interface IUser {
+    name: string,
+    age: number|string,
+    isCat: boolean
+    hasOwner?: boolean;
+}
+
+interface IAnimal {
+    name: string;
+    kind: string;
+}
+
+const tom:IUser = {
+    name: 'Tom',
+    age: 40,
+    isCat: true,
+    hasOwner: true,
+}
+
+const jerry:IUser = {
+    name: 'Jerry',
+    age: 35,
+    isCat: false,
+}
+
+const userData: IUser[] = [tom,jerry];
+
+console.log(userData);
+
+
+
+
+
+  return (
+    <div className="lesson-container">
+      <h4>Lesson06</h4>
+    </div>
+  );
+}
+
+export default Lesson06;
