d4c652f34a045736f3653d3bf70d96334f6ea448
Valerii Kuchynskyi <valeryk@hotmail.com>
2024-07-17 17:23:56 +0200

    FormGender resolved

--- src/components/formGender/FormGender.tsx
+++ src/components/formGender/FormGender.tsx
@@ -3,13 +3,12 @@ import styles from "./formGender.module.css";
 import { useFormik } from "formik";
 
 interface IFormGender {
-  name: string
-  }
+  name: string;
+}
 
 export default function FormGender() {
   const [gender, setGender] = useState("");
   const [nameOut, setNameOut] = useState("");
-  const [isFill, setIsFill] = useState(false);
 
   const formik = useFormik({
     initialValues: {
@@ -17,24 +16,13 @@ export default function FormGender() {
     } as IFormGender,
 
     onSubmit: (values: IFormGender, { resetForm }) => {
-        setIsFill(false);
-      
-      
-
       fetch(`https://api.genderize.io/?name=${values.name}`)
         .then((res) => res.json())
         .then((data) => {
           setGender(data.gender);
           setNameOut(data.name);
-          setIsFill(true);
           resetForm();
-          
-        })
-        .catch(() => {
-          setIsFill(false);
         });
-
-
     },
   });
 
@@ -52,9 +40,9 @@ export default function FormGender() {
       <br />
       {nameOut && (
         <span>
-         Name: {nameOut} <br />
-         Gender: {gender}
-      </span>
+          Name: {nameOut} <br />
+          Gender: {gender}
+        </span>
       )}
     </form>
   );
