9ab8eb9111f87b8186669686b7095753a10e93e1
Valerii Kuchynskyi <valeryk@hotmail.com>
2024-07-24 17:09:59 +0200

    toDo list pending

--- src/components/toDoList/ToDoList.tsx
+++ src/components/toDoList/ToDoList.tsx
@@ -1,20 +1,47 @@
+import { useState } from "react";
 import { TaskRemoveButton, TaskItem, TasksContainer, InputButton, InputForm, InputFormContainer, Title, ToDoListContainer } from "./styles";
+import { useFormik } from "formik";
+import { v4 } from "uuid";
 
+interface IFormValues{
+    inputData: string
+}
 
 export default function ToDoList() {
-  
+
+    const [taskList, setTaslList] = useState<string[]>([]);
+        
+    const formik = useFormik( {
+        initialValuses:{
+            inputData: ''
+        } as IFormValues,
+        onSubmit : (values: IFormValues, {resetForm}) => {
+            console.log(values);
+            setTaslList((preValue) => [...preValue, values.inputData])
+            resetForm ()
+        }
+        })
+        console.log(formik);
+        
+
+        const taskListOutput = taskList.map((task) => {
+            return (
+                <TasksContainer>
+                <TaskItem>{task}</TaskItem>
+                <TaskRemoveButton>✖️</TaskRemoveButton>
+            </TasksContainer>
+            )
+        })
+
   
     return (
     <ToDoListContainer>
         <Title>ToDo list from consultation 5</Title>
-        <InputFormContainer>
-            <InputForm placeholder="input task"></InputForm>
-            <InputButton>add task</InputButton>
+        <InputFormContainer onSubmit={formik.handleSubmit}>
+            <InputForm  onChange={formik.handleChange} name='inputData' value={formik.values.inputData} placeholder="input task"></InputForm>
+            <InputButton type="submit"> add task</InputButton>
         </InputFormContainer>
-        <TasksContainer>
-            <TaskItem></TaskItem>
-            <TaskRemoveButton>✖️</TaskRemoveButton>
-        </TasksContainer>
+        {taskListOutput}
     </ToDoListContainer>
   )
 }
